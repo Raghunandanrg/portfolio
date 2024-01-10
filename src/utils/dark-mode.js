8754f237b05670f16ff3861e06dc85d173c71924
Raghunandanrg <raghunandanrg1999@gmail.com>
2024-01-10 17:13:04 +0530

    first initial state

---
+++ src/utils/dark-mode.js
@@ -0,0 +1,20 @@
+const dark_mode=()=>{
+const themeToggleButton=document.querySelectorAll('#theme-toggle');
+const theme=localStorage.getItem('theme');
+//onmount
+theme && document.body.classList.add(theme)
+themeToggleButton.forEach(element => {
+    element.addEventListener('click',()=>{
+        document.body.classList.toggle('light-mode');
+        if(document.body.classList.contains('light-mode')){
+        localStorage.setItem('theme','light-mode');
+        }
+        else{
+            localStorage.removeItem('theme')
+            document.body.removeAttribute('class')
+        }
+    })
+});
+};
+
+export default dark_mode;
